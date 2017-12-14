e927bb3c856d3fed2f910ab86b540f861427e935
k123321141 <kk123321141@gamil.com>
2017-12-15 06:04:53 +0800

    finish hw6 with zip

--- hw6/hw6_r06922115/script.py
+++ hw6/hw6_r06922115/script.py
@@ -2,10 +2,15 @@ import sys,os,string
 import subprocess
 import time
 from subprocess import Popen, PIPE
-#/home/k99/lib/pin-3.5-97503-gac534ca30-gcc-linux/pin -t /home/k99/lib/pin-3.5-97503-gac534ca30-gcc-linux/source/tools/ManualExamples/obj-intel64/inscount0.so -- ./break
+from os.path import expanduser,join
 
-pin ='/home/k99/lib/pin-3.5-97503-gac534ca30-gcc-linux/pin'
-insc = '/home/k99/lib/pin-3.5-97503-gac534ca30-gcc-linux/source/tools/ManualExamples/obj-intel64/inscount0.so'
+"""
+    Hard coding path
+"""
+home = expanduser("~")
+pin = join(home,'lib','pin-3.5-97503-gac534ca30-gcc-linux','pin')
+insc = join(home,'lib','pin-3.5-97503-gac534ca30-gcc-linux','source','tools','ManualExamples','obj-intel64','inscount0.so')
+
 command = '%s -t %s -- ./break' % (pin,insc)
 threshold = 50
 def read_ints():
@@ -27,10 +32,10 @@ def try_str(s):
 def main():
 
     #alphabet = string.ascii_uppercase + string.ascii_lowercase + '1234567890' + ' -=[],./`~!@#$%^&*()_+{}:<>?'
-    alphabet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~ \t\n\r\x0b\x0c'
+    alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~1234567890 \t\n\r\x0b\x0c'
     t = len(alphabet)
     maximum = 0
-    s = 'CTF{PinBJ1\tX:z7t(Tt'
+    #s = 'CTF{PinADXAnInterfaceforCustomizableDebuggingwithDynamicInstrumentation'
     s = ''
     while True:
         correct_char = ''
@@ -51,7 +56,7 @@ def main():
                     break
                 maximum = max(maximum , count)
             else:
-                print 'correct string : [%s]' % s
+                print 'correct string : [%s]' % (s+c)
                 return
         if correct_char == '':
             print 'char is not in alphabet'
